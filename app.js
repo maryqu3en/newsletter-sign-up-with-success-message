5707d674dd8952f0c028e3bf2a10e7aea4916059
soubih meriem <soubih.meriem@gmail.com>
2023-05-30 21:43:48 +0100

    last commit vol2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   let form = document.querySelector("form");
+  let signInPage = document.getElementById("sign-in-block");
   let successMessage = document.getElementById("success-message-block");
   let emailBox = document.getElementById("email-box");
   let userEmail = document.getElementById("user-email");
@@ -7,10 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
   successMessage.style.display = "none";
 
-  // Add submit event listener to the form
   form.addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form submission
-
     let email = emailBox.value;
     if (!validateEmail(email)) {
       emailBox.classList.add("invalid");
@@ -19,11 +18,11 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Hide the sign-in block and display the success message
-    document.getElementById("sign-in-block").style.display = "none";
+    signInPage.style.display = "none";
     successMessage.style.display = "block";
     // Update user email in success message
     userEmail.textContent = email;
-    userEmail.style.fontWeight = '700';
+    userEmail.style.fontWeight = "700";
 
     // Reset email box value and remove error styling
     emailBox.value = "";
@@ -32,10 +31,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   dismissButton.addEventListener("click", function () {
     successMessage.style.display = "none";
-    document.getElementById("sign-in-block").style.display = "block";
+    signInPage.style.display = "block";
   });
 
-  // Email validation function
   function validateEmail(email) {
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
